5df56476e30eb465ff21b5e4b7e0fe38b602fa80
Chris Stevens <splooge@pwned.com>
2020-06-12 12:22:37 -0700

    Cleaning up build directory

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2020.06.09.05.17',
+    VERSION: '2020.06.12.19.21',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
